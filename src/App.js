5ecda3e482a35dbf62706ba97fb929dc1274d27e
Armin Blaze <box_for_spam@mail.ru>
2020-02-14 05:04:08 +0300

    2, 3 - Добавил Навигацию и Роутер

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
+import About from 'pages/About';
+import Profile from 'pages/Profile';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
+    <Navbar />
     <div className='container pt-4'>
-      <h1>Hello</h1>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/profile/:name' component={Profile} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+const About = () => {
+  return (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">Информация</h1>
+        <p className="lead">Версия приложения <b>1.0.0</b></p>
+      </div>
+    </div>
+  );
+}
+ 
+export default About;
