d5bcca5c8431d0822e6e138d8119b1d254c896b4
Armin Blaze <box_for_spam@mail.ru>
2020-02-17 04:30:11 +0300

    4 - Добавил Поиск и Карточки на главной

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,10 +2,10 @@ import React from 'react';
  
 const About = () => {
   return (
-    <div className="jumbotron">
-      <div className="container">
-        <h1 className="display-4">Информация</h1>
-        <p className="lead">Версия приложения <b>1.0.0</b></p>
+    <div className='jumbotron'>
+      <div className='container'>
+        <h1 className='display-4'>Информация</h1>
+        <p className='lead'>Версия приложения <b>1.0.0</b></p>
       </div>
     </div>
   );
