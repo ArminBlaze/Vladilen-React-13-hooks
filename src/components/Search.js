edc5ad3c9c28c28db8f1525cf3aad6e22d135cb0
Armin Blaze <box_for_spam@mail.ru>
2020-02-20 13:20:07 +0300

    Search - обработчик на форме

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,24 +4,23 @@ import { AlertContext } from 'context/alert/AlertContext';
 const Search = () => {
   const {showAlert} = useContext(AlertContext);
 
-  const handleInput = (e) => {
-    
-    if(e.key !== 'Enter') {
-      console.log(e.key);
-      return;
-    }
+  const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(e.target.elements);
+    let value = e.target.name.value.trim();
+    if(value.length === 0) showAlert('Вы ничего не ввели', 'danger')
 
-    if(e.target.value.length === 0) showAlert('Вы ничего не ввели', 'danger')
+    console.log(value);
+    
   }
 
   return (
-    <form className='form-group'>
+    <form className='form-group' onSubmit={handleSubmit}>
       <input 
         type="text"
+        name='name'
         className='form-control'
         placeholder='Введите ник пользователя...'
-        onKeyPress={handleInput}
       />
     </form>
   );
